f2b9a45ba47ac804bf71f070727a344e7e39315f
sami2020pro <64816951+sami2020pro@users.noreply.github.com>
2020-10-01 02:46:50 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { RandomMeal, Meal } from './components';
+
+function App() {
+  return (
+    <div className="App">
+        <RandomMeal />
+
+        <Meal />
+        <Meal />
+    </div>
+  );
+}
+
+export default App;
